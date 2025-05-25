056408a50529946b8fbc9e207bd95ce3fb3be049
josgal314 <jmgalvez314@gmail.com>
2025-05-24 22:53:35 -0400

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -24,6 +24,9 @@ VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10,
 
 
 # define card class
+
+
+
 class Card:
     def __init__(self, suit, rank):
         if (suit in SUITS) and (rank in RANKS):
@@ -47,7 +50,29 @@ class Card:
         card_loc = (CARD_CENTER[0] + CARD_SIZE[0] * RANKS.index(self.rank), 
                     CARD_CENTER[1] + CARD_SIZE[1] * SUITS.index(self.suit))
         canvas.draw_image(card_images, card_loc, CARD_SIZE, [pos[0] + CARD_CENTER[0], pos[1] + CARD_CENTER[1]], CARD_SIZE)
-        
+  
+
+
+# We suggest modeling a hand as a list of Card objects that are stored in a field in the Hand object
+
+
+
+#The __init__method should initialize the Hand object to have an empty list of Card objects. 
+
+
+#The 
+#add_card
+# should append a Card object to this list of cards. 
+
+
+
+#The 
+#__str__
+# should return a string representation of a Hand object in a human-readable form
+
+
+
+
 # define hand class
 class Hand:
     def __init__(self):
@@ -62,11 +87,43 @@ class Hand:
     def get_value(self):
         # count aces as 1, if the hand has an ace, then add 10 to hand value if it doesn't bust
         pass	# compute the value of the hand, see Blackjack video
+    
+    
+#Implement the get_value method for the 
+#Hand class. You should use the provided VALUE dictionary to look up the value of a single 
+#card in conjunction with the logic explained 
+#in the video lecture for this project to compute the value of a hand. 
+    
+    
    
     def draw(self, canvas, pos):
         pass	# draw a hand on the canvas, use the draw method for cards
  
         
+    
+#We suggest modeling a deck of cards as list of cards. 
+
+    
+#You can generate this list using a pair of nested 
+#for loops or a list comprehension.     
+
+
+#Remember to use the 
+#Card initializer to create your cards.
+
+
+
+# Use 
+#random.shuffle() to shuffle this deck of cards.
+
+
+#Remember that the deck is randomized after shuffling, so the output of the 
+#testing template should match the output in the comments in form but not in exact value.
+
+
+
+
+    
 # define deck class 
 class Deck:
     def __init__(self):
@@ -83,6 +140,32 @@ class Deck:
         pass	# return a string representing the deck
 
 
+    
+    
+#Implement the handler for a "Deal" button that shuffles the deck and 
+#deals the two cards to both the dealer and the player.    
+    
+    
+    
+    
+#The event handler 
+#deal for this button should shuffle the deck (stored as a global variable), 
+#create new player and dealer hands (stored as global variables), and add two cards to each hand.    
+
+
+
+
+
+#To transfer a card from the deck to a hand, you should use the 
+#deal_card method of the 
+#Deck class and the 
+#add_card method of 
+#Hand class in combination. 
+
+
+
+
+
 
 #define event handlers for buttons
 def deal():
@@ -91,6 +174,11 @@ def deal():
     # your code goes here
     
     in_play = True
+    
+    
+    
+    
+    
 
 def hit():
     pass	# replace with your code below
@@ -98,6 +186,15 @@ def hit():
     # if the hand is in play, hit the player
    
     # if busted, assign a message to outcome, update in_play and score
+    
+    
+#Implement the handler for a "Hit" button. If the value of the hand is less 
+#than or equal to 21, clicking this button adds an extra card to player's hand. 
+#If the value exceeds 21 after being hit, print "You have busted".    
+    
+    
+    
+    
        
 def stand():
     pass	# replace with your code below
@@ -105,6 +202,15 @@ def stand():
     # if hand is in play, repeatedly hit dealer until his hand has value 17 or more
 
     # assign a message to outcome, update in_play and score
+    
+    
+#Implement the handler for a "Stand" button. If the player has busted, remind the 
+#player that they have busted. Otherwise, repeatedly hit the dealer until his hand 
+#has value 17 or more (using a while loop). If the dealer busts, let the player know. 
+#Otherwise, compare the value of the player's and dealer's hands. If the value of the 
+#player's hand is less than or equal to the dealer's hand, the dealer wins. Otherwise 
+#the player has won. Remember the dealer wins ties in our version.    
+
 
 # draw handler    
 def draw(canvas):
@@ -127,6 +233,11 @@ frame.set_draw_handler(draw)
 
 # get things rolling
 deal()
+
+#In our version of Blackjack, a hand is automatically dealt to the player 
+#and dealer when the program starts. In particular, the program template 
+#includes a call to the deal()function during initialization. 
+
 frame.start()
 
 
